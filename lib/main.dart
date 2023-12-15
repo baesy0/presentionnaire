42392a15e03b268bf948b4945840cef96bc850a5
baesy0 <bsy2658@gmail.com>
2023-12-15 17:07:21 +0900

    선물지 사이트 UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -43,19 +44,42 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             Container(
-              color: Colors.white,
+              color: Color.fromARGB(255, 226, 184, 248),
               width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.8,
+              height: MediaQuery.of(context).size.height,
               child: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      '관념적인 개념을 몸으로 체화하여 익히는 프로그램입니다.\n소마틱스, 정신과 몸의 연결성에 철학적 바탕을 두고,\n분석심리학에 이론적인 배경을 두고 있습니다.',
+                      'PRESENTIONNAIRE',
                       style: GoogleFonts.notoSans(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w100,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 20,
                       color: Colors.black,  
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 32.0), // 추가된 여백
+                    Text(
+                      '선물지',
+                      style: GoogleFonts.notoSans(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w100,
+                      letterSpacing: 20,
+                      color: Colors.black,  
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 60.0),
+                    Text(
+                      'ALL RIGHTS RESERVED',
+                      style: GoogleFonts.notoSans(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w300,
+                      letterSpacing: 5,
+                      color: Color.fromARGB(255, 109, 109, 109),  
                       ),
                       textAlign: TextAlign.center,
                     ),
@@ -65,9 +89,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Container(
-              color: Colors.blueGrey,
+              color: Colors.white,
               width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.8,
+              height: MediaQuery.of(context).size.height,
               child: Stack(
                 children: [
                   Center(
@@ -75,34 +99,24 @@ class _MyHomePageState extends State<MyHomePage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          '구성요소',
+                          '지금, 눈 앞의 너를 깊이 알려는 마음.',
                           style: GoogleFonts.notoSans(
                             fontWeight: FontWeight.w200,
-                            fontSize: 40.0,
-                            letterSpacing: 20,
+                            fontSize: 30.0,
+                            letterSpacing: 10,
                             color: Colors.black,
                             height: 2.0,
                           ),
                         ),
                         Text(
-                          '아래 요소를 상황과 주제에 맞게 배치합니다.',
+                          '받는 이의 삶에 스며들어 오래 머무는 선물.',
                           style: GoogleFonts.notoSans(
-                            fontWeight: FontWeight.w100,
-                            fontSize: 20.0,
-                            letterSpacing: 1.0,
+                            fontWeight: FontWeight.w200,
+                            fontSize: 30.0,
+                            letterSpacing: 10,
                             color: Colors.black,
+                            height: 2.0,
                           ),
-                        ),
-                        SizedBox(height: 30), // 원하는 간격 조절
-                        Text(
-                          '명상 / 호흡(Pranayama) / 요가\n움직임 (Authentic Movement, Contact Beyond Contact, Contact Improvisation)\n대화 / 글',
-                          style: GoogleFonts.notoSans(
-                          fontWeight: FontWeight.w100,
-                          fontSize: 15.0,
-                          letterSpacing: 1.0,
-                          color: Colors.black,
-                          ),
-                          textAlign: TextAlign.center,
                         ),
                       ],
                     ),
@@ -111,49 +125,28 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Container(
-              color: Colors.white,
+              color: Color.fromARGB(255, 226, 184, 248),
               width: double.infinity,
               height: MediaQuery.of(context).size.height * 0.8,
-              child: Stack(
-                children: [
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '주제',
-                          style: GoogleFonts.notoSans(
-                            fontWeight: FontWeight.w200,
-                            fontSize: 40.0,
-                            letterSpacing: 20,
-                            color: Colors.black,
-                            height: 2.0,
-                          ),
-                        ),
-                        Text(
-                          '움직임 워크샵을 통해 다루는 주제입니다.',
-                          style: GoogleFonts.notoSans(
-                            fontWeight: FontWeight.w100,
-                            fontSize: 20.0,
-                            letterSpacing: 1.0,
-                            color: Colors.black,
-                          ),
-                        ),
-                        SizedBox(height: 30), // 원하는 간격 조절
-                        Text(
-                          '그라운딩 / 센터링 / 관계 & 연결 / 플로잉\n자기신뢰 / 자기 신뢰에 기반한 타인에 대한 신뢰 / 감정 (분노, 슬픔 ...)',
-                          style: GoogleFonts.notoSans(
-                          fontWeight: FontWeight.w100,
-                          fontSize: 15.0,
-                          letterSpacing: 1.0,
-                          color: Colors.black,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 버튼 클릭 시 수행할 작업
+                  },
+                   child: Text(
+                    '선물지\n다운받기',
+                    textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                    style: TextStyle(color: Colors.white), // 텍스트 색상을 하얀색으로 설정
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple, // 버튼 색상
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20), // 버튼 패딩
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
+                ),
               ),
             ),
           ],
